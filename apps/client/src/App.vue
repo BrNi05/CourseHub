be8e95b600582172ff87534e7e6de87c0a92ed19
BrNi05 <szocsbarnabas8@gmail.com>
2026-03-03 16:44:43 +0100

    MISC: add UI framework

---
+++ apps/client/src/App.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+<template>
+  <main class="min-h-screen grid place-items-center bg-zinc-950 text-zinc-100">
+    <h1 class="text-2xl font-medium">CourseHub</h1>
+  </main>
+</template>
---
+++ apps/client/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import UnoCSS from 'unocss/vite';
+import path from 'node:path';
+
+export default defineConfig({
+  plugins: [vue(), UnoCSS()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  },
+});
